1bc9d1b562f8230b9add856ae51d0992a7ad9192
Lev <levblt1@gmail.com>
2023-09-21 15:34:38 +0300

    extract server initiation and running client command to methods

--- server.py
+++ server.py
@@ -1,21 +1,42 @@
 import socket
+from typing import Type
 
 # TODO: change to relative/package? have a common package?
 from common import commands
 from common.exceptions import InvalidArgumentException
 from common.protocol import get_msg, send_msg, parse_command, validate_command
+from common.types import Command
 
 
 # TODO: have this be configurable in env/as a cli argument
 CLIENT_TIMEOUT_SECONDS = 10
 
 
-def main() -> None:
+def init_server_socket() -> socket.socket:
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # TODO: have the port be configurable in env/as a cli argument
     server_socket.bind(("0.0.0.0", 8080))
     server_socket.listen()
 
+    return server_socket
+
+
+def run_client_command(
+    command_class: Type[Command], arguments: list[str], client_socket: socket.socket
+) -> None:
+    try:
+        result = command_class(*arguments).run()
+    except InvalidArgumentException as e:
+        send_msg(client_socket, str(e))
+        return
+    else:
+        send_msg(client_socket, result)
+    finally:
+        client_socket.close()
+
+
+def main() -> None:
+    server_socket = init_server_socket()
     done = False
 
     try:
@@ -40,19 +61,18 @@ def main() -> None:
                     command_name,
                     commands,
                     arguments,
-                    invalid_command_callback=lambda error_msg: send_msg(client_socket, error_msg),
-                    invalid_arguments_callback=lambda error_msg: send_msg(client_socket, error_msg),
+                    invalid_command_callback=lambda error_msg: send_msg(
+                        client_socket, error_msg
+                    ),
+                    invalid_arguments_callback=lambda error_msg: send_msg(
+                        client_socket, error_msg
+                    ),
                 )
                 if not valid_command:
                     continue
 
-                try:
-                    result = command_class(*arguments).run()
-                except InvalidArgumentException as e:
-                    send_msg(client_socket, str(e))
-                    continue
+                run_client_command(command_class, arguments, client_socket)
 
-            send_msg(client_socket, result)
     finally:
         print("Exiting...")
         server_socket.close()
