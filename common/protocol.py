3ec49194e968eb3982e1218d09622fe74dd65cc3
Lev <levblt1@gmail.com>
2023-09-21 13:21:30 +0300

    add method to try to receive until timeout

--- common/protocol.py
+++ common/protocol.py
@@ -3,16 +3,26 @@ from __future__ import annotations
 import socket
 
 
+def _try_recv_until_timeout(s: socket.socket, amount: int) -> tuple[bool, bytes]:
+    try:
+        msg = s.recv(amount)
+    except TimeoutError:
+        return False, b"Timeout"
+    return True, msg
+
+
 def get_msg(s: socket.socket) -> tuple[bool, str]:
-    length_str = s.recv(4).decode()
-    if not length_str.isdigit():
+    success, length_bytes = _try_recv_until_timeout(s, 4)
+    length_str = length_bytes.decode()
+
+    if not success or not length_str.isdigit():
         # TODO: return invalid length received
         # TODO: cleanup socket?
         return False, "Error"
 
     length = int(length_str)
-    message = s.recv(length)
-    return True, message.decode()
+    success, message = _try_recv_until_timeout(s, length)
+    return success, message.decode()
 
 
 def send_msg(s: socket.socket, msg: str) -> None:
