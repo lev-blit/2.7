83ef97d4f68880f4137b369cf0086ba5a285a57e
Lev <levblt1@gmail.com>
2023-09-21 14:41:21 +0300

    implement COPY command

--- common/commands.py
+++ common/commands.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import shutil
 from typing import Any
 
 from common.exceptions import InvalidArgumentException, InvalidArgumentListException
@@ -48,3 +49,30 @@ class DELETE(Command):
         except OSError as e:
             return False, str(e)
         return True, f"Successfully deleted {self.path}"
+
+
+class COPY(Command):
+    def __init__(self, *args: Any) -> None:
+        super().__init__("COPY")
+        self.validate_argument_list(*args)
+        self.source, self.dest = args
+
+    @classmethod
+    def validate_argument_list(cls, *args: Any) -> None:
+        if not len(args) == 2:
+            raise InvalidArgumentListException("COPY only supports two arguments")
+        source, dest = args
+        if source == dest:
+            raise InvalidArgumentListException("Dest argument must be different from source argument")
+
+    def validate_pre_run(self) -> None:
+        if not os.path.isfile(self.source):
+            raise InvalidArgumentException(f"The given source argument \"{self.source}\" is not a file")
+
+    def run(self) -> tuple[bool, str]:
+        self.validate_pre_run()
+        try:
+            shutil.copy(self.source, self.dest)
+        except OSError as e:
+            return False, str(e)
+        return True, f"Successfully {self.source} to {self.dest}"
