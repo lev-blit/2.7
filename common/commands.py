94057e4d2fdfbfb8e6b221294bb3219f4eb75888
Lev <levblt1@gmail.com>
2023-09-21 14:23:45 +0300

    implement DELETE command

--- common/commands.py
+++ common/commands.py
@@ -24,3 +24,27 @@ class DIR(Command):
     def run(self) -> tuple[bool, list[str]]:
         self.validate_pre_run()
         return True, glob.glob(fr"{self.path}\*.*")
+
+
+class DELETE(Command):
+    def __init__(self, *args: Any) -> None:
+        super().__init__("DELETE")
+        self.validate_argument_list(*args)
+        self.path = args[0]
+
+    @classmethod
+    def validate_argument_list(cls, *args: Any) -> None:
+        if not len(args) == 1:
+            raise InvalidArgumentListException("DELETE only supports one argument")
+
+    def validate_pre_run(self) -> None:
+        if not os.path.isfile(self.path):
+            raise InvalidArgumentException(f"The given path argument \"{self.path}\" is not a file")
+
+    def run(self) -> tuple[bool, str]:
+        self.validate_pre_run()
+        try:
+            os.remove(self.path)
+        except OSError as e:
+            return False, str(e)
+        return True, f"Successfully deleted {self.path}"
