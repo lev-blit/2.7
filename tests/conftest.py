35f2f2ac393b4e367c1f019f3b3280f6fe07bd2c
Lev <levblt1@gmail.com>
2023-09-30 16:03:56 +0300

    make the server process persistent to all tests to avoid process creation time

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,25 +8,15 @@ from typing import Iterator
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def server_port() -> int:
     return 9090
 
 
-@pytest.fixture()
-def server_process(server_port: int) -> Iterator[subprocess.Popen[bytes]]:
-    process = subprocess.Popen(
-        [sys.executable, "-m", "remote_tech.server", "--port", str(server_port)],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    yield process
-    if process.returncode is None:
-        process.terminate()
-
-
-@pytest.fixture
-def server_process_tmpdir(server_port: int) -> Iterator[tuple[subprocess.Popen[bytes], str]]:
+@pytest.fixture(scope="session")
+def server_process_tmpdir(
+    server_port: int,
+) -> Iterator[tuple[subprocess.Popen[bytes], str]]:
     old_path = os.getcwd()
     with tempfile.TemporaryDirectory() as tmpdir:
         os.chdir(tmpdir)
--- tests/server_test.py
+++ tests/server_test.py
@@ -1,29 +1,37 @@
 import socket
 import subprocess
+import sys
 
 import pytest
+from conftest import client_socket
 
 from remote_tech.common.protocol import get_msg
 from remote_tech.common.protocol import send_msg
 
 
-@pytest.mark.usefixtures("server_process")
+@pytest.mark.usefixtures("server_process_tmpdir")
 def test_invalid_message(client_socket_fixture: socket.socket) -> None:
     client_socket_fixture.send(b"invalid message")
     response = get_msg(client_socket_fixture)
     assert response == (True, b"Invalid message sent")
 
 
-@pytest.mark.usefixtures("server_process")
+@pytest.mark.usefixtures("server_process_tmpdir")
 def test_invalid_command(client_socket_fixture: socket.socket) -> None:
     send_msg(client_socket_fixture, b"invalid message")
     response = get_msg(client_socket_fixture)
     assert response == (True, b"Received invalid command - invalid")
 
 
-def test_exit(server_process: subprocess.Popen, client_socket_fixture: socket.socket) -> None:
-    send_msg(client_socket_fixture, "EXIT")
-    response = get_msg(client_socket_fixture)
+def test_exit() -> None:
+    process = subprocess.Popen(
+        [sys.executable, "-m", "remote_tech.server", "--port", "6060"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    c = client_socket(6060)
+    send_msg(c, "EXIT")
+    response = get_msg(c)
     assert response == (True, b"Server shutting down...")
-    server_process.wait(0.5)
-    assert server_process.returncode == 0
+    process.wait(0.5)
+    assert process.returncode == 0
